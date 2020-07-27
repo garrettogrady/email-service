ef2282d11078d350bc5f2d91b6eb49198e068880
Garrett <garrettogrady@gmail.com>
2020-07-27 06:45:04 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Container, Divider } from 'semantic-ui-react'
 import './App.css';
+import ProductCard from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container textAlign='center'>
+            <ProductCard/>
+        </Container>
+
     </div>
   );
 }
